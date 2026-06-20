28fec19fa77b35b038f8c8877367bbb68f960ca9
Allen Alosiyas <allenalosiyas638@gmail.com>
2026-06-20 14:38:28 +0530

    fix auth routes

--- frontend/src/api/authService.js
+++ frontend/src/api/authService.js
@@ -2,13 +2,13 @@ import API from "../../api/api";
 
 // 🔥 REGISTER
 export const registerUser = async (data) => {
-  const res = await API.post("/auth/register", data);
+  const res = await API.post("/api/auth/register", data);
   return res.data;
 };
 
 // 🔥 LOGIN
 export const loginUser = async (data) => {
-  const res = await API.post("/auth/login", data);
+  const res = await API.post("/api/auth/login", data);
   return res.data;
 };
 
